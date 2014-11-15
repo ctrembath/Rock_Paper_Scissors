82bfe375070832434d3e5f0de00a7c298d88af30
ctrembath <claretrembath@ymail.com>
2014-11-15 14:04:41 +0000

    game can have a player

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,10 @@ class Game
   attr_accessor :player, :robot
 
   def initialize
-    player = nil
+    @player = nil
   end
 
-  def receive_shot(rps)
-    player.shoots(rps)
+  def add_player(player)
+    self.player = player
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,19 @@
+require "game"
 
-# has one player
+describe Game do
+
+  let(:game) { Game.new }
+  let(:player) { double :player }
+
+it "can have a player" do
+  game.add_player(player)
+  expect(game.player).to eq(player)
+
+end
+
+
+end
+ # has one player
 
 # has one robot player
 
